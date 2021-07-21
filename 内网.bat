c797906f18c463cf3cb9e7f92d1a27fcd7e5c9e2
lei9527 <63177411+lei9527@users.noreply.github.com>
2021-07-21 22:20:41 +0800

    Update 内网.bat

--- 内网.bat
+++ 内网.bat
@@ -1,3 +1,3 @@
 %1 start "" mshta vbscript:CreateObject("Shell.Application").ShellExecute("cmd.exe","/c ""%~s0"" ::","","runas",1)(window.close)&&exit
-netsh interface ip set address name="ÒÔÌ«Íø" source=dhcp
-netsh interface ip set dns name="ÒÔÌ«Íø" source=dhcp
+netsh interface ip set address name="ä»¥å¤ªç½‘" source=dhcp
+netsh interface ip set dns name="ä»¥å¤ªç½‘" source=dhcp
