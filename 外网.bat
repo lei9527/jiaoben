f976da5048d6c0ecff57d4fffb9a2debaac6cb3e
lei9527 <63177411+lei9527@users.noreply.github.com>
2021-07-21 22:21:12 +0800

    Update 外网.bat

--- 外网.bat
+++ 外网.bat
@@ -1,4 +1,3 @@
 %1 start "" mshta vbscript:CreateObject("Shell.Application").ShellExecute("cmd.exe","/c ""%~s0"" ::","","runas",1)(window.close)&&exit
-netsh interface ip set address name="ÒÔÌ«Íø" source=static addr=192.168.1.33 mask=255.255.255.0 gateway=192.168.1.100
-netsh interface ip set dns name="ÒÔÌ«Íø" source=static addr=114.114.114.114
-
+netsh interface ip set address name="ä»¥å¤ªç½‘" source=static addr=192.168.1.33 mask=255.255.255.0 gateway=192.168.1.100
+netsh interface ip set dns name="ä»¥å¤ªç½‘" source=static addr=114.114.114.114
